d22787423e361590da9e9e7464146cf70d98719a
liron navon <lironavon42@gmail.com>
2018-08-19 16:28:27 +0300

    test

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 const express = require('express');
+const isArray = require('lodash/isArray');
 const app = express();
 const ytdl = require('youtube-dl');
+const port = process.env.PORT || 3000;
 
+// show that the server is healthy
 app.get('/', (req, res) => {
-    res.send('Hello ' + process.platform)
-})
-
-app.get('/video/:video', (req, res) => {
-    const video = req.params.video;
-    const url = `https://www.youtube.com/watch?v=${video}`;
-    ytdl.getInfo(url, ['--youtube-skip-dash-manifest'], function (err, info) {
-        if(err) {
-            console.log('failed')
-           res.send(err);
-        } else {
-            console.log('yays')
-            console.log(info.formats)
-           res.send(info.formats)
-        }
-    })
-
-    // res.send('Hello World!');
+    res.json({
+        os: process.platform,
+        port,
+        healthy: true
+    });
 });
 
-const port = process.env.PORT || 3000;
+//return info for specified video
+app.get('/info/:video', (req, res) => {
+    const video = req.params.video;
+    const inputFlags = req.params.flags;
+    const filters = req.params.filters;
+
+    getInfo(video, inputFlags)
+        .then(info => {
+            if (isArray(filters)) {
+                info.formats = filterFormats(filters, info.formats);
+            }
+            res.json(info)
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+// filter to only keep specific formats
+function filterFormats(formatsToKeep, formats) {
+    return formats.filter(format => {
+        return formatsToKeep.includes(format.ext);
+    })
+}
+
+// return the info using ytdl
+function getInfo(videoId, flags = ['--youtube-skip-dash-manifest']) {
+    return new Promise((resolve, reject) => {
+        const url = `https://www.youtube.com/watch?v=${videoId}`;
+        ytdl.getInfo(url, flags, (err, info) => {
+            err ? reject(err) : resolve(info);
+        })
+    })
+}
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
