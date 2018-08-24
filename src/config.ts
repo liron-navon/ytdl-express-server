193b985ff2d73a302c9b606795c916d1990f0178
liron navon <lironavon42@gmail.com>
2018-08-24 11:09:41 +0300

    proxying

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 export const PORT =  process.env.PORT || 3000;
-
 export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyAtDAu-6fyq0Qa0pzYHKSVj45HB-6tH4Vs';
-
 export const YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3';
+export const API_VERSION = 'v1';
--- src/helpers/proxify.ts
+++ src/helpers/proxify.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express';
+import { API_VERSION } from 'src/config';
 
-'express';
+// convert normal url to be proxy url
 export function proxify(request: Request, urlToProxify: string) {
-    return `${request.protocol}://${request.get('host')}/proxy/${urlToProxify}`;
+    return `${request.protocol}://${request.get('host')}/${API_VERSION}/proxy/${urlToProxify}`;
 }
